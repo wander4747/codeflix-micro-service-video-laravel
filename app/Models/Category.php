debe35721f28baf167788cbab56ca6ce74bbb56b
wander4747 <wander.douglas14@gmail.com>
2021-06-12 18:41:11 -0300

    fix is_active model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,9 +13,10 @@ class Category extends Model
 
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
+    protected $casts = [
+        'id' => 'string',
+        'is_active' => 'boolean'
+    ];
     public $incrementing = false;
     protected $keyType = 'string';
-    protected $casts = [
-        'id' => 'string'
-    ];
 }
--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -13,6 +13,10 @@ class Genre extends Model
 
     protected $fillable = ['name', 'is_active'];
     protected $dates = ['deleted_at'];
+    protected $casts = [
+        'id' => 'string',
+        'is_active' => 'boolean'
+    ];
     public $incrementing = false;
     protected $keyType = 'string';
 }
