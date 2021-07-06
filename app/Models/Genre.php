2fac74d0628a5fb9a952afe38eb5d45e7387d697
wander4747 <wander.douglas14@gmail.com>
2021-07-06 20:08:01 -0300

    update genre model and controller

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -19,4 +19,9 @@ class Genre extends Model
     ];
     public $incrementing = false;
     protected $keyType = 'string';
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class)->withTrashed();
+    }
 }
---
+++ tests/Traits/TestRelations.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Traits;
+
+
+trait TestRelations
+{
+
+}
