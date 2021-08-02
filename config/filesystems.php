ce00cf920a2f4b2c20b83660655851fab063ccd6
wander4747 <wander.douglas14@gmail.com>
2021-08-02 18:03:03 -0300

    create config Google Cloud Storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,6 +57,17 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
+
+        'gcs' => [
+            'driver' => 'gcs',
+            'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'),
+            'key_file' => storage_path('credentials/google/' . env('GOOGLE_CLOUD_KEY_FILE', null)),
+            'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'your-bucket'),
+            'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
+            'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
+            'visibility' => 'private', // optional: public|private
+        ],
+
     ],
 
     /*
