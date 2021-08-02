a2773f28174db211e74e7f0acc939af6f4238184
wander4747 <wander.douglas14@gmail.com>
2021-08-02 18:29:16 -0300

    creating production upload tests

--- tests/Prod/Models/Traits/UploadFilesProdTest.php
+++ tests/Prod/Models/Traits/UploadFilesProdTest.php
@@ -8,22 +8,27 @@ use Tests\TestCase;
 use Tests\Stubs\Models\UploadFilesStub;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use Tests\Traits\TestProd;
+use Tests\Traits\TestStorages;
 
 
 class UploadFilesProdTest extends TestCase
 {
+    use TestStorages, TestProd;
 
     private $obj;
 
     protected function setUp(): void
     {
         parent::setUp();
+        $this->skipTestIfNotProd();
         $this->obj = new UploadFilesStub();
+        \Config::set('filesystems.default', 'gcs');
+        $this->deleteAllFiles();
     }
 
     public function testUploadFile()
     {
-        Storage::fake();
         $file = UploadedFile::fake()->create('video.mp4');
         $this->obj->uploadFile($file);
         Storage::assertExists("1/{$file->hashName()}");
@@ -31,7 +36,6 @@ class UploadFilesProdTest extends TestCase
 
     public function testUploadFiles()
     {
-        Storage::fake();
         $file1 = UploadedFile::fake()->create('video1.mp4');
         $file2 = UploadedFile::fake()->create('video2.mp4');
         $this->obj->uploadFiles([$file1, $file2]);
@@ -41,7 +45,6 @@ class UploadFilesProdTest extends TestCase
 
     public function testDeleteOldFiles()
     {
-        \Storage::fake();
         $file1 = UploadedFile::fake()->create('video1.mp4')->size(1);
         $file2 = UploadedFile::fake()->create('video2.mp4')->size(1);
         $this->obj->uploadFiles([$file1, $file2]);
@@ -56,7 +59,6 @@ class UploadFilesProdTest extends TestCase
 
     public function testDeleteFile()
     {
-        \Storage::fake();
         $file = UploadedFile::fake()->create('video.mp4');
         $this->obj->uploadFile($file);
         $filename = $file->hashName();
@@ -71,50 +73,11 @@ class UploadFilesProdTest extends TestCase
 
     public function testDeleteFiles()
     {
-        \Storage::fake();
         $file1 = UploadedFile::fake()->create('video1.mp4');
         $file2 = UploadedFile::fake()->create('video2.mp4');
         $this->obj->uploadFiles([$file1, $file2]);
         $this->obj->deleteFiles([$file1->hashName(), $file2]);
         \Storage::assertMissing("1/{$file1->hashName()}");
         \Storage::assertMissing("1/{$file2->hashName()}");
-    }
-
-    public function testExtractFiles()
-    {
-        $attributes = [];
-        $files = UploadFilesStub::extractFiles($attributes);
-        $this->assertCount(0, $attributes);
-        $this->assertCount(0, $files);
-
-        $attributes = ['file1' => 'test'];
-        $files = UploadFilesStub::extractFiles($attributes);
-        $this->assertCount(1, $attributes);
-        $this->assertEquals(['file1' => 'test'], $attributes);
-        $this->assertCount(0, $files);
-
-        $attributes = ['file1' => 'test', 'file2' => 'test'];
-        $files = UploadFilesStub::extractFiles($attributes);
-        $this->assertCount(2, $attributes);
-        $this->assertEquals(['file1' => 'test', 'file2' => 'test'], $attributes);
-        $this->assertCount(0, $files);
-
-        $file1 = UploadedFile::fake()->create('video1.mp4');
-        $attributes = ['file1' => $file1, 'other' => 'test'];
-        $files = UploadFilesStub::extractFiles($attributes);
-        $this->assertCount(2, $attributes);
-        $this->assertEquals(['file1' => $file1->hashName(), 'other' => 'test'], $attributes);
-        $this->assertEquals([$file1], $files);
-
-        $file2 = UploadedFile::fake()->create('video1.mp4');
-        $attributes = ['file1' => $file1, 'file2' => $file2, 'other' => 'test'];
-        $files = UploadFilesStub::extractFiles($attributes);
-        $this->assertCount(3, $attributes);
-        $this->assertEquals([
-            'file1' => $file1->hashName(),
-            'file2' => $file2->hashName(),
-            'other' => 'test'
-        ], $attributes);
-        $this->assertEquals([$file1, $file2], $files);
     }
 }
--- tests/Traits/TestStorages.php
+++ tests/Traits/TestStorages.php
@@ -1,8 +1,16 @@
 <?php
 
-namespace Traits;
+namespace Tests\Traits;
 
 trait TestStorages
 {
-
+    protected function deleteAllFiles()
+    {
+        $dirs = \Storage::directories();
+        foreach ($dirs as $dir){
+            $files = \Storage::files($dir);
+            \Storage::delete($files);
+            \Storage::deleteDirectory($dir);
+        }
+    }
 }
